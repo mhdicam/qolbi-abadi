b8e17b1d9f0f4cfa4f9cd623137b5f4fa95b29da
Hisyam <79328867+mhdicam@users.noreply.github.com>
2023-05-23 06:03:08 +0700

    Update _sidebar.php

--- _sidebar.php
+++ _sidebar.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="bo" class="brand-link">
-      <img src="assets-login/images/qa.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3">
+      <img src="assets-login/images/logo-lana.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3">
       <span class="brand-text font-weight-light">POS - Inventory</span>
     </a>
 
